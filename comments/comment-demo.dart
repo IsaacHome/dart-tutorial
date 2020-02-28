4b458a18b2dc420d52652c77b202e4b49ef9535b
isaachome <g.isaacvhm@gmail.com>
2020-02-28 11:37:51 +0630

    dart coments

---
+++ comments/comment-demo.dart
@@ -0,0 +1,10 @@
+void main() {
+  // thie is first line of code
+  print("Testing Comments");
+
+  /*  
+    multiple line comments 
+    learning a lot comments 
+  */
+  print("Second line of Code");
+}
